ecede2f4d8692f68a0fa76b6afe915f9860db5e9
kenechi <nsudekenechi2@gmail.com>
2024-03-22 14:39:28 +0100

    Finished myplan.php

--- admin/myplan.php
+++ admin/myplan.php
@@ -25,7 +25,8 @@ require_once "./includes/header.php";
                                     <li><a href="#" class="btn btn-primary"><span>Withdraw</span> <em
                                                 class="icon ni ni-arrow-long-right d-none d-sm-inline-block"></em></a>
                                     </li>
-                                    <li><a href="#" class="btn btn-white btn-light"><span>Invest More</span> <em
+                                    <li><a href="./invest.php" class="btn btn-white btn-light"><span>Invest
+                                                More</span> <em
                                                 class="icon ni ni-arrow-long-right d-none d-sm-inline-block"></em></a>
                                     </li>
                                     <li class="opt-menu-md dropdown">
@@ -52,16 +53,25 @@ require_once "./includes/header.php";
                                 <div class="row gy-gs">
                                     <div class="col-lg-5">
                                         <div class="nk-iv-wg3">
+                                            <?php
+                                            $query = "SELECT * FROM users WHERE id = '$userid'";
+                                            $res = mysqli_query($conn, $query);
+
+                                            ?>
                                             <div class="nk-iv-wg3-title">Total Balance</div>
                                             <div class="nk-iv-wg3-group  flex-lg-nowrap gx-4">
                                                 <div class="nk-iv-wg3-sub">
                                                     <div class="nk-iv-wg3-amount">
-                                                        <div class="number">18,752.84 <small
-                                                                class="currency currency-usd">USD</small></div>
+                                                        <div class="number">
+                                                            <?= number_format($res->fetch_column(5), 2); ?> <small
+                                                                class="currency currency-usd">USD</small>
+                                                        </div>
                                                     </div>
-                                                    <div class="nk-iv-wg3-subtitle">Available Balance</div>
+                                                    <div class="nk-iv-wg3-subtitle">Available Balance
+
+                                                    </div>
                                                 </div>
-                                                <div class="nk-iv-wg3-sub">
+                                                <!-- <div class="nk-iv-wg3-sub">
                                                     <span class="nk-iv-wg3-plus text-soft"><em
                                                             class="icon ni ni-plus"></em></span>
                                                     <div class="nk-iv-wg3-amount">
@@ -70,7 +80,7 @@ require_once "./includes/header.php";
                                                     <div class="nk-iv-wg3-subtitle">Locked Balance <em
                                                             class="icon ni ni-info-fill" data-bs-toggle="tooltip"
                                                             data-bs-placement="right" title="You can't use"></em></div>
-                                                </div>
+                                                </div> -->
                                             </div>
                                         </div>
                                     </div><!-- .col -->
@@ -81,17 +91,38 @@ require_once "./includes/header.php";
                                                     title="Current Month Profit"></em></div>
                                             <div class="nk-iv-wg3-group flex-md-nowrap g-4">
                                                 <div class="nk-iv-wg3-sub-group gx-4">
+                                                    <?php
+                                                    $currMonth = date("M");
+                                                    $query = "SELECT SUM(deposits.amount * (plans.increase/100)) FROM deposits
+                                                    JOIN plans
+                                                    ON deposits.plan = plans.id
+                                                     WHERE deposits.user = '$userid' AND deposits.last_profit LIKE '%$currMonth%'";
+                                                    $res = mysqli_query($conn, $query);
+
+                                                    ?>
                                                     <div class="nk-iv-wg3-sub">
                                                         <div class="nk-iv-wg3-amount">
-                                                            <div class="number">5,394.62</div>
+                                                            <div class="number">
+                                                                <?= number_format($res->fetch_column(), 2); ?>
+                                                            </div>
                                                         </div>
                                                         <div class="nk-iv-wg3-subtitle">Total Profit</div>
                                                     </div>
                                                     <div class="nk-iv-wg3-sub">
+                                                        <?php
+                                                        $currDay = date("d-M-Y");
+                                                        $query = "SELECT SUM(deposits.amount * (plans.increase/100)) FROM deposits
+                                                                   JOIN plans
+                                                                   ON deposits.plan = plans.id
+                                                                    WHERE deposits.user = '$userid' AND deposits.last_profit LIKE '$currDay%'";
+                                                        $res = mysqli_query($conn, $query);
+                                                        ?>
                                                         <span class="nk-iv-wg3-plus text-soft"><em
                                                                 class="icon ni ni-plus"></em></span>
                                                         <div class="nk-iv-wg3-amount">
-                                                            <div class="number-sm">1,50.05</div>
+                                                            <div class="number-sm">
+                                                                <?= number_format($res->fetch_column(), 2); ?>
+                                                            </div>
                                                         </div>
                                                         <div class="nk-iv-wg3-subtitle">Today Profit</div>
                                                     </div>
@@ -106,7 +137,7 @@ require_once "./includes/header.php";
                                     </div><!-- .col -->
                                 </div><!-- .row -->
                             </div><!-- .card-inner -->
-                            <div class="card-inner">
+                            <!-- <div class="card-inner">
                                 <ul class="nk-iv-wg3-nav">
                                     <li><a href="#"><em class="icon ni ni-notes-alt"></em> <span>Go to
                                                 Transaction</span></a></li>
@@ -117,92 +148,95 @@ require_once "./includes/header.php";
                                     <li><a href="#"><em class="icon ni ni-help"></em> <span>Investment Tips</span></a>
                                     </li>
                                 </ul>
-                            </div><!-- .card-inner -->
+                            </div> -->
+
+                            <!-- .card-inner -->
                         </div><!-- .card-inner-group -->
                     </div><!-- .card -->
                 </div><!-- .nk-block -->
                 <div class="nk-block nk-block-lg">
                     <div class="nk-block-head-sm">
                         <div class="nk-block-head-content">
-                            <h5 class="nk-block-title">Active Plan <span class="count text-base">(2)</span></h5>
+                            <?php
+                            $query = "SELECT * FROM deposits
+                            JOIN plans
+                            ON deposits.plan = plans.id
+                             WHERE deposits.active = true AND deposits.user='$userid'";
+                            $res = mysqli_query($conn, $query);
+                            ?>
+                            <h5 class="nk-block-title">Active Plan <span class="count text-base">
+                                    <?= "($res->num_rows)"; ?>
+                                </span></h5>
                         </div>
                     </div>
                     <div class="nk-iv-scheme-list">
-                        <div class="nk-iv-scheme-item">
-                            <div class="nk-iv-scheme-icon is-running">
-                                <em class="icon ni ni-update"></em>
-                            </div>
-                            <div class="nk-iv-scheme-info">
-                                <div class="nk-iv-scheme-name">Silver - Daily 4.76% for 21 Days</div>
-                                <div class="nk-iv-scheme-desc">Invested Amount - <span class="amount">$250</span></div>
-                            </div>
-                            <div class="nk-iv-scheme-term">
-                                <div class="nk-iv-scheme-start nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Start Date</span>
-                                    <span class="nk-iv-scheme-value date">Nov 04, 2019</span>
+                        <div class="nk-iv-scheme-list">
+                            <?php
+                            while ($row = $res->fetch_assoc()) {
+                                ?>
+                                <div class="nk-iv-scheme-item">
+                                    <div class="nk-iv-scheme-icon is-done">
+                                        <em class="icon ni ni-offer"></em>
+                                    </div>
+                                    <div class="nk-iv-scheme-info">
+                                        <div class="nk-iv-scheme-name">
+                                            <?= $row['name']; ?> - Daily
+                                            <?= $row['increase']; ?>% for
+                                            <?= $row['days']; ?> Days
+                                        </div>
+                                        <div class="nk-iv-scheme-desc">Invested Amount - <span class="amount">
+                                                $
+                                                <?= $row['amount']; ?>
+                                            </span></div>
+                                    </div>
+                                    <div class="nk-iv-scheme-term">
+                                        <div class="nk-iv-scheme-start nk-iv-scheme-order">
+                                            <span class="nk-iv-scheme-label text-soft">Start Date</span>
+                                            <span class="nk-iv-scheme-value date">
+                                                <?= date_format(new DateTime($row['start_date']), 'M d, Y'); ?>
+
+                                            </span>
+                                        </div>
+                                        <div class="nk-iv-scheme-end nk-iv-scheme-order">
+                                            <span class="nk-iv-scheme-label text-soft">End Date</span>
+                                            <span class="nk-iv-scheme-value date">
+                                                <?= date_format(new DateTime($row['end_date']), 'M d, Y'); ?>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="nk-iv-scheme-amount">
+                                        <div class="nk-iv-scheme-amount-a nk-iv-scheme-order">
+                                            <span class="nk-iv-scheme-label text-soft">Total Return</span>
+                                            <?php
+                                            $total = $row['amount'] + ($row['amount'] * ($row['days'] * ($row['increase'] / 100)))
+                                                ?>
+                                            <span class="nk-iv-scheme-value amount">$
+                                                <?= number_format($total, 2); ?>
+                                            </span>
+                                        </div>
+                                        <div class="nk-iv-scheme-amount-b nk-iv-scheme-order">
+                                            <span class="nk-iv-scheme-label text-soft">Net Profit Earn</span>
+                                            <span class="nk-iv-scheme-value amount"> <span class="amount-ex">~
+                                                    $
+                                                    <?= number_format($total - $row['amount'], 2); ?>
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <!-- <div class="nk-iv-scheme-more">
+                                    <a class="btn btn-icon btn-lg btn-round btn-trans"
+                                        href="html/invest/scheme-details.html"><em class="icon ni ni-forward-ios"></em></a>
+                                </div> -->
                                 </div>
-                                <div class="nk-iv-scheme-end nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">End Date</span>
-                                    <span class="nk-iv-scheme-value date">Nov 25, 2019</span>
-                                </div>
-                            </div>
-                            <div class="nk-iv-scheme-amount">
-                                <div class="nk-iv-scheme-amount-a nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Total Return</span>
-                                    <span class="nk-iv-scheme-value amount">$ 499.99</span>
-                                </div>
-                                <div class="nk-iv-scheme-amount-b nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Net Profit Earn</span>
-                                    <span class="nk-iv-scheme-value amount">$ 97.95 <span class="amount-ex">~
-                                            $152.04</span></span>
-                                </div>
-                            </div>
-                            <div class="nk-iv-scheme-more">
-                                <a class="btn btn-icon btn-lg btn-round btn-trans"
-                                    href="html/invest/scheme-details.html"><em class="icon ni ni-forward-ios"></em></a>
-                            </div>
-                            <div class="nk-iv-scheme-progress">
-                                <div class="progress-bar" data-progress="25"></div>
-                            </div>
-                        </div><!-- .nk-iv-scheme-item -->
-                        <div class="nk-iv-scheme-item">
-                            <div class="nk-iv-scheme-icon is-running">
-                                <em class="icon ni ni-update"></em>
-                            </div>
-                            <div class="nk-iv-scheme-info">
-                                <div class="nk-iv-scheme-name">Silver - Daily 4.76% for 21 Days</div>
-                                <div class="nk-iv-scheme-desc">Invested Amount - <span class="amount">$1,250</span>
-                                </div>
-                            </div>
-                            <div class="nk-iv-scheme-term">
-                                <div class="nk-iv-scheme-start nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Start Date</span>
-                                    <span class="nk-iv-scheme-value date">Oct 30, 2019</span>
-                                </div>
-                                <div class="nk-iv-scheme-end nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">End Date</span>
-                                    <span class="nk-iv-scheme-value date">Nov 19, 2019</span>
-                                </div>
-                            </div>
-                            <div class="nk-iv-scheme-amount">
-                                <div class="nk-iv-scheme-amount-a nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Total Return</span>
-                                    <span class="nk-iv-scheme-value amount">$ 2,500</span>
-                                </div>
-                                <div class="nk-iv-scheme-amount-b nk-iv-scheme-order">
-                                    <span class="nk-iv-scheme-label text-soft">Net Profit Earn</span>
-                                    <span class="nk-iv-scheme-value amount">$ 1145.25 <span class="amount-ex">~
-                                            $105.75</span></span>
-                                </div>
-                            </div>
-                            <div class="nk-iv-scheme-more">
-                                <a class="btn btn-icon btn-lg btn-round btn-trans"
-                                    href="html/invest/scheme-details.html"><em class="icon ni ni-forward-ios"></em></a>
-                            </div>
-                            <div class="nk-iv-scheme-progress">
-                                <div class="progress-bar" data-progress="90"></div>
-                            </div>
-                        </div><!-- .nk-iv-scheme-item -->
+                                <?php
+                            }
+                            ?>
+
+                            <!-- .nk-iv-scheme-item -->
+
+                        </div>
+                        <!-- .nk-iv-scheme-item -->
+
                     </div><!-- .nk-iv-scheme-list -->
                 </div><!-- .nk-block -->
                 <div class="nk-block nk-block-lg">
@@ -218,8 +252,7 @@ require_once "./includes/header.php";
                             ?>
                             <div class="nk-block-head-content">
                                 <h5 class="nk-block-title">Recently End <span class="count text-base">
-                                        (
-                                        <?= $res->num_rows; ?> )
+                                        <?= "($res->num_rows)"; ?>
                                     </span></h5>
                             </div>
                             <div class="nk-block-head-content">
